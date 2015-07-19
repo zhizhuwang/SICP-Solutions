e8394d912615d02e7f367012fe8a939e1ef6678a
zhizhuwang <zhizhuwang@gmail.com>
2015-07-19 16:01:56 +0800

    2.25 solution 3 added

--- 2.25.rkt
+++ 2.25.rkt
@@ -25,3 +25,32 @@
 
 
 (1 (2 (3 (4 (5 (6 7))))))
+> (define z (list 1 (list 2 (list 3 (list 4 (list 5 (list 6 7)))))))
+> z
+'(1 (2 (3 (4 (5 (6 7))))))
+> (cdr z)
+> (cadr z)
+'(2 (3 (4 (5 (6 7)))))
+> (cdadr z)
+'((3 (4 (5 (6 7)))))
+> (cadadr z)
+'(3 (4 (5 (6 7))))
+> (car (cadadr z))
+3
+> (cdr (cadadr z))
+'((4 (5 (6 7))))
+> (cadr (cadadr z))
+'(4 (5 (6 7)))
+> (cdadr (cadadr z))
+'((5 (6 7)))
+> (cadadr (cadadr z))
+'(5 (6 7))
+> (cdr (cadadr (cadadr z)))
+'((6 7))
+> (cadr (cadadr (cadadr z)))
+'(6 7)
+> (cdadr (cadadr (cadadr z)))
+'(7)
+> (cadadr (cadadr (cadadr z)))
+7
+> 
