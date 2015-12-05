ed95e35b8824b5af523c144bf37e8f899ec506c1
zhizhuwang <zhizhuwang@gmail.com>
2015-12-05 14:58:35 +0800

    Create 2.28.rkt

---
+++ 2.28.rkt
@@ -0,0 +1,15 @@
+(define (fringe ls)
+    (cond ((not (pair? ls)) (list ls))
+          ((null? (cdr ls)) (fringe (car ls)))
+          (else (append (fringe (car ls)) (fringe (cdr ls)))
+           )
+          )
+  )
+  
+(define x (list (list 1 2) (list 3 4)))
+
+> (fringe x)
+'(1 2 3 4)
+
+> (fringe 1)
+'(1)
