c6f5be3bcc863cc2cd101443f752478a53b8594a
zhizhuwang <zhizhuwang@gmail.com>
2015-12-10 21:13:47 +0800

    map implementation

--- 2.30.rkt
+++ 2.30.rkt
@@ -4,3 +4,17 @@
       '()
       (cons (proc (car items))
             (map proc (cdr items)))))
+
+(define (square x) (* x x))
+
+
+
+
+(define (square-tree tree)
+  (map (lambda (element)  
+         (if (pair? element)
+             (square-tree element)
+             (square element))) 
+       tree
+  ))
+
