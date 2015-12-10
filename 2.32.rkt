d485b47e8a65222f8e78b5efe05b9e00d26462e9
zhizhuwang <zhizhuwang@gmail.com>
2015-12-10 22:06:54 +0800

    give explanation

--- 2.32.rkt
+++ 2.32.rkt
@@ -5,3 +5,9 @@
       (let ((rest (subsets (cdr s))))
         (append rest (map (lambda (n) (cons (car s) n)) rest)))))
 
+;; explanation
+;; (subset s) -> (subset [h,t]) where h -> car s, t -> cdr s
+;; if rest -> (subset t), the elements of (subset s) have two classes,
+;; the other ones that have h, and the other ones that have h,
+;; rest is the result of first class, 
+;; for the second class, we insert h to every elements of rest
