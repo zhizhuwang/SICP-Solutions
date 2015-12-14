0409cd5918cc1e5c43b39e10fb271ec20755d5a1
zhizhuwang <zhizhuwang@gmail.com>
2015-12-14 15:31:54 +0800

    Update 2.33.rkt

--- 2.33.rkt
+++ 2.33.rkt
@@ -7,3 +7,6 @@
                     (accumulate fn initial (cdr sequence)))))
 (define (map p sequence)
 	(accumulate (lambda (x y) (cons (p x) y)) (list ) sequence))
+	
+(define (append2 seq1 seq2)
+  (accumulate cons seq2 seq1))
