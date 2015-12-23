9772906ec7f0c3b8f7f2a320fabb3d4fd384a4cf
zhizhuwang <zhizhuwang@gmail.com>
2015-12-23 21:53:47 +0800

    update ex2.39

--- 2.39.rkt
+++ 2.39.rkt
@@ -8,3 +8,12 @@
 
 (define (reverse-left sequence)
   (foldl cons '() sequence))
+
+
+(define (foldr fun initial sequence)
+  (if (null? sequence)
+      initial
+      (fun (car sequence) (foldr fun initial (cdr sequence)))))
+
+(define (reverse-right sequence)
+  (foldr (lambda (x y) (append y (list x))) '() sequence))
