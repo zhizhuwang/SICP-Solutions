8324726496e7eb3ca1cd75df11d44b4aff9875cb
zhizhuwang <zhizhuwang@gmail.com>
2015-12-29 17:40:56 +0800

    Update 2.50.rkt

--- 2.50.rkt
+++ 2.50.rkt
@@ -1,3 +1,21 @@
+(define (frame-coord-map frame)
+  (lambda (v)
+    (add-vect
+      (origin-frame frame)
+        (add-vect (scale-vect (xcor-vect v)
+                              (edge1-frame frame))
+                  (scale-vect (ycor-vect v)
+                              (esge2-frame frame))))))
+
+(define (transfor-printer painter origin coner1 corner2)
+  (lambda (frame)
+    (let ((m (frame-coord-map frame)))
+      (let ((new-origin (m origin)))
+        (painter
+            (make-frame new-origin
+                        (sub-vect (m corner1) new-origin)
+                        (sub-vect (m corner2) new-origin)))))))
+                        
 (define (flip-horiz painter)
   (let ((split-point (make-vect 0.5 0)))
     (let ((painter-left (transform-painter painter
@@ -18,5 +36,13 @@
                       (make-vect 1 1)
                       (make-vect 0 1)
                       (make-vect 1 0))))
+        (lambda (frame)
+          (new-painter frame))))
+
+(define (rotate-270 painter)
+  (let ((new-painter (transform-painter painter
+                      (make-vect 0 1)
+                      (make-vect 0 1)
+                      (make-vect 1 1))))
         (lambda (frame)
           (new-painter frame))))
