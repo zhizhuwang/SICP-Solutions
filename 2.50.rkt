3aa621868941e09b06a297121d646e17a2f23c98
zhizhuwang <zhizhuwang@gmail.com>
2015-12-30 22:34:08 +0800

    modify flip-horiz

--- 2.50.rkt
+++ 2.50.rkt
@@ -17,18 +17,13 @@
                         (sub-vect (m corner2) new-origin)))))))
                         
 (define (flip-horiz painter)
-  (let ((split-point (make-vect 0.5 0)))
-    (let ((painter-left (transform-painter painter
-                    (make-vect (0.5 0))
-                    (make-vect (0.5 1))
-                    (make-vect (0 0))))
-          (painter-right (transform-painter painter
-                    (make-vect (0.5 0))
-                    (make-vect (1 0))
-                    (make-vect (0.5 1)))))
+    (let ((new-painter (transform-painter painter
+                    (make-vect (1.0 0.0))
+                    (make-vect (0.0 0.0))
+                    (make-vect (1.0 1.0)))))
           (lambda (frame)
-            (painter-left frame)
-            (painter-left frame)))))
+            (painter-left frame))))
+
 
 
 (define (rotate-180 painter)
