97dada4e5cf9c67d0f23313ea9b2a15b73f8fae7
zhizhuwang <zhizhuwang@gmail.com>
2016-01-04 22:29:24 +0800

    update 2.3.2 example of differentiation

--- 2.3.2.rkt
+++ 2.3.2.rkt
@@ -20,9 +20,9 @@
 (define (same-variable? v1 v2)
   (and (variable? v1) (variable? v2) (eq? v1 v2)))
 
-(define (make-sum a1 a2) (list '+ a1 a2))
+;; (define (make-sum a1 a2) (list '+ a1 a2))
 
-(define (make-product m1 m2) (list '* m1 m2))
+;; (define (make-product m1 m2) (list '* m1 m2))
 
 (define (sum? x)
   (and (pair? x) (eq? (car x) '+)))
@@ -48,6 +48,27 @@
 ;; '(+ (* (* x y) (+ 1 0)) (* (+ (* x 0) (* 1 y)) (+ x 3)))
  
 
+(define (make-sum a1 a2)
+  (cond ((=number? a1 0) a2)
+        ((=number? a2 0) a1)
+        ((and (number? a1) (number? a2)) (+ a1 a2))
+        (else (list '+ a1 a2))))
 
+(define (=number? exp num)
+       (and (number? exp) (= exp num)))
+
+(define (make-product m1 m2)
+  (cond ((or (=number? m1 0) (=number? m2 0)) 0)
+        ((=number? m1 1) m2)
+        ((=number? m2 1) m1)
+        ((and (number? m1) (number? m2)) (* m1 m2))
+        (else (list '* m1 m2))))
+
+;; > (deriv '(+ x 3) 'x)
+;; 1
+;; > (deriv '(* x y) 'x)
+;; 'y
+;; > (deriv '(* (* x y) (+ x 3)) 'x)
+;; '(+ (* x y) (* y (+ x 3)))
 
 
