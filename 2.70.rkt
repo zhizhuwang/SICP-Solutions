eb5479dd6916e3437f11c2cd8d805235908e74bc
zhizhuwang <zhizhuwang@gmail.com>
2016-01-16 23:05:19 +0800

    fix bug of ex2.70

--- 2.70.rkt
+++ 2.70.rkt
@@ -91,17 +91,17 @@
 
 (define (encode-symbol symbol tree)
     (cond ((leaf? tree) '())
-          ((symbol-in-set? symbol (symbols (left-branch tree))) 
+          ((symbol-in-set? symbol (left-branch tree)) 
            (cons 0 (encode-symbol symbol (left-branch tree))))
-          ((symbol-in-set? symbol (symbols (right-branch tree))) 
+          ((symbol-in-set? symbol (right-branch tree)) 
            (cons 1 (encode-symbol symbol (right-branch tree))))
           (else (error "error message"))))
 
-(define (symbol-in-set? symbol set)
+(define (symbol-in-set? symbol tree)
   (not
    (false? (findf 
             (lambda (s) (eq? symbol s)) 
-            set))))
+            (symbols tree)))))
 
 
 ;; > (encode '(A) sample-tree)
@@ -154,19 +154,13 @@
 
 > (define eight-symbols-tree
     (generate-huffman-tree
-     '((A 2) (BOOM 1) (GET 2) (JOB 2) (NA 16) (SHA 3) (YIP 9) (WAH 1))))
+     '((A 1) (BOOM 1) (GET 2) (JOB 2) (NA 16) (SHA 3) (YIP 9) (WAH 1))))
 
 > eight-symbols-tree
 '((leaf NA 16)
   ((leaf YIP 9)
-   (((leaf A 2)
-     ((leaf WAH 1) (leaf BOOM 1) (WAH BOOM) 2)
-     (A WAH BOOM)
-     4)
-    ((leaf SHA 3)
-     ((leaf JOB 2) (leaf GET 2) (JOB GET) 4)
-     (SHA JOB GET)
-     7)
+   (((leaf A 2) ((leaf WAH 1) (leaf BOOM 1) (WAH BOOM) 2) (A WAH BOOM) 4)
+    ((leaf SHA 3) ((leaf JOB 2) (leaf GET 2) (JOB GET) 4) (SHA JOB GET) 7)
     (A WAH BOOM SHA JOB GET)
     11)
    (YIP A WAH BOOM SHA JOB GET)
@@ -177,8 +171,12 @@
 
 
 > (encode '(GET A JOB) eight-symbols-tree )
-;;'(1 1 1 1 1 1 1 0 0 1 1 1 1 0)
+;;'(1 1 0 0 1 1 1 1 0 1 1 1 1 1)
 
-
-
+> (encode '(GET) eight-symbols-tree )
+;; '(1 1 0 0)
+> (encode '(A) eight-symbols-tree )
+;; '(1 1 1 1 0)
+> (encode '(JOB) eight-symbols-tree )
+;; '(1 1 1 1 1)
 
