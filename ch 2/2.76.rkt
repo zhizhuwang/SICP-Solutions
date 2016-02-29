2075926eea74636528f6d706a7ad0188d49f75fc
zhizhuwang <zhizhuwang@gmail.com>
2016-02-29 22:17:16 +0800

    ex2.76 created

---
+++ ch 2/2.76.rkt
@@ -0,0 +1,5 @@
+Explicit dispatch: when this strategy is used, name conflicting must be avoided, and when new operations were added, all the existing generic opreations must be changed.So this strategy is appropriate for systems that in which operations or new types are added. With this strategy, the system could not be constructed additively.
+
+Data-Directed: with the support of installation of package, this strategy can meke new generic operations and new types to be added additively.
+
+Message Passing: with this strategy, new date types can be added easily. But it is not appropriate for the systems that generic operations are often added.
