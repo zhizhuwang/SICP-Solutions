22dffaffd03f1a06825b0a868f0fb93dfe84f69b
zhizhuwang <zhizhuwang@gmail.com>
2016-03-08 17:26:55 +0800

    Update 2.32.rkt

--- ch 2/2.32.rkt
+++ ch 2/2.32.rkt
@@ -8,6 +8,6 @@
 ;; explanation
 ;; (subset s) -> (subset [h,t]) where h -> car s, t -> cdr s
 ;; if rest -> (subset t), the elements of (subset s) have two classes,
-;; the other ones that have h, and the other ones that have h,
+;; the ones that don't have h, and the other ones that have h,
 ;; rest is the result of first class, 
 ;; for the second class, we insert h to every elements of rest
